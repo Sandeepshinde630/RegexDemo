1b218600323c162e01928f47e39bede60cce1c1c
unknown <sandeepshinde630@gmail.com>
2021-08-23 12:14:45 +0530

    feat:Added Regex Demo Program

---
+++ RegexPatternMakingDemo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace RegexPatternMakingDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("PinCode Check!");
+            Patterns patterns = new Patterns();
+            Console.WriteLine(patterns.validatePinCode("442406"));
+
+        }
+    }
+}
